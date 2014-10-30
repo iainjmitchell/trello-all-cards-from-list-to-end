4524fc70fcafb9c7a8319fcfd0552ff1dc916228
iainjmitchell <iainjmitchell@gmail.com>
2014-10-30 16:59:11 +0000

    connecting to trello with public key

--- tests/AllCardsFromListToEndTests.rb
+++ tests/AllCardsFromListToEndTests.rb
@@ -2,6 +2,20 @@ require 'test/unit'
 require 'rspec-expectations'
 require 'securerandom'
 
+require 'TrelloFactory'
+require 'TrelloCredentials'
+
+class TrelloBoardCards
+	include AgileTrello
+
+	def initialize(parameters)
+		trello_factory = parameters[:trello_factory].nil? ? TrelloFactory.new : parameters[:trello_factory]
+		trello_credentials = TrelloCredentials.new(parameters[:public_key], nil)
+		trello_factory.create(trello_credentials)
+	end
+end
+
+
 class AllCardsFromListToEndTests < Test::Unit::TestCase
 	def test_user_connects_to_trello_with_public_key
 		public_key = SecureRandom.uuid
